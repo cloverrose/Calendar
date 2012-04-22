ebb199a50f4634a807e3a9538c75d378b6dafbae
cloverrose <clover.plus.rose@gmail.com>
2012-04-22 18:18:49 +0900

    refactoring

--- mysite/cal/views.py
+++ mysite/cal/views.py
@@ -50,6 +50,39 @@ def named_month(pMonthNumber):
     """
     return date(1900, pMonthNumber, 1).strftime('%B')
 
+def event_filter(events,lYear,lMonth):
+    lCalendarFromMonth = datetime(lYear, lMonth, 1)
+    lCalendarToMonth = datetime(lYear, lMonth, monthrange(lYear, lMonth)[1])
+    return events.filter(day__gte=lCalendarFromMonth, day__lte=lCalendarToMonth)
+
+def make_dict(pYear,pMonth):
+    lYear = int(pYear)
+    lMonth = int(pMonth)
+    lPreviousMonth = lMonth - 1
+    lPreviousYear=lYear
+    if lPreviousMonth == 0:
+        lPreviousMonth = 12
+        lPreviousYear  = lYear-1
+    lNextMonth = lMonth + 1
+    lNextYear  = lYear
+    if lNextMonth == 13:
+        lNextMonth = 1
+        lNextYear  = lYear+1
+    lYearAfterThis = lYear + 1
+    lYearBeforeThis = lYear - 1
+
+    return {'Month' : lMonth,
+            'MonthName' : named_month(lMonth),
+            'Year' : lYear,
+            'PreviousMonth' : lPreviousMonth,
+            'PreviousMonthName' : named_month(lPreviousMonth),
+            'PreviousYear' : lPreviousYear,
+            'NextMonth' : lNextMonth,
+            'NextMonthName' : named_month(lNextMonth),
+            'NextYear' : lNextYear,
+            'YearBeforeThis' : lYearBeforeThis,
+            'YearAfterThis' : lYearAfterThis,
+            }
 def index(request):
     """
     Show calendar of events this month
@@ -63,78 +96,31 @@ def calendar(request, pYear, pMonth):
     """
     lYear = int(pYear)
     lMonth = int(pMonth)
-    lCalendarFromMonth = datetime(lYear, lMonth, 1)
-    lCalendarToMonth = datetime(lYear, lMonth, monthrange(lYear, lMonth)[1])
-    lEvents = Event.objects.filter(day__gte=lCalendarFromMonth, day__lte=lCalendarToMonth)
+    lEvents = event_filter(Event.objects.all(),lYear,lMonth)
     lCalendar = EventCalendar(lEvents).formatmonth(lYear, lMonth)
-    lPreviousMonth = lMonth - 1
-    lPreviousYear=lYear
-    if lPreviousMonth == 0:
-        lPreviousMonth = 12
-        lPreviousYear  = lYear-1
-    lNextMonth = lMonth + 1
-    lNextYear  = lYear
-    if lNextMonth == 13:
-        lNextMonth = 1
-        lNextYear  = lYear+1
-    lYearAfterThis = lYear + 1
-    lYearBeforeThis = lYear - 1
+    
+    dict=make_dict(pYear,pMonth)
+    dict['Calendar']=mark_safe(lCalendar)
+    return render_to_response('cal/month.html',dict)
 
-    return render_to_response('cal/month.html', {'Calendar' : mark_safe(lCalendar),
-                                                'Month' : lMonth,
-                                                'MonthName' : named_month(lMonth),
-                                                'Year' : lYear,
-                                                'PreviousMonth' : lPreviousMonth,
-                                                'PreviousMonthName' : named_month(lPreviousMonth),
-                                                'PreviousYear' : lPreviousYear,
-                                                'NextMonth' : lNextMonth,
-                                                'NextMonthName' : named_month(lNextMonth),
-                                                'NextYear' : lNextYear,
-                                                'YearBeforeThis' : lYearBeforeThis,
-                                                'YearAfterThis' : lYearAfterThis,
-                                               })
 def index_user(request, user_name):
     """
     Show calendar of events this month
     """
     lToday = datetime.now()
     return calendar_user(request, lToday.year, lToday.month, user_name)
+
 def calendar_user(request, pYear, pMonth, user_name):
     """
     Show calendar of events for specified month and year
     """
     lYear = int(pYear)
     lMonth = int(pMonth)
-    lCalendarFromMonth = datetime(lYear, lMonth, 1)
-    lCalendarToMonth = datetime(lYear, lMonth, monthrange(lYear, lMonth)[1])
     luser=User.objects.get(name=user_name)
     userEvents=luser.events.all()
-    lEvents = userEvents.filter(day__gte=lCalendarFromMonth, day__lte=lCalendarToMonth)
+    lEvents = event_filter(userEvents,lYear,lMonth)
     lCalendar = EventCalendar(lEvents).formatmonth(lYear, lMonth)
-    lPreviousMonth = lMonth - 1
-    lPreviousYear=lYear
-    if lPreviousMonth == 0:
-        lPreviousMonth = 12
-        lPreviousYear  = lYear-1
-    lNextMonth = lMonth + 1
-    lNextYear  = lYear
-    if lNextMonth == 13:
-        lNextMonth = 1
-        lNextYear  = lYear+1
-    lYearAfterThis = lYear + 1
-    lYearBeforeThis = lYear - 1
-
-    return render_to_response('cal/month_user.html', {'Calendar' : mark_safe(lCalendar),
-                                                      'Month' : lMonth,
-                                                      'MonthName' : named_month(lMonth),
-                                                      'Year' : lYear,
-                                                      'PreviousMonth' : lPreviousMonth,
-                                                      'PreviousMonthName' : named_month(lPreviousMonth),
-                                                      'PreviousYear' : lPreviousYear,
-                                                      'NextMonth' : lNextMonth,
-                                                      'NextMonthName' : named_month(lNextMonth),
-                                                      'NextYear' : lNextYear,
-                                                      'YearBeforeThis' : lYearBeforeThis,
-                                                      'YearAfterThis' : lYearAfterThis,
-                                                      'user_name' : user_name,
-                                                      })
+    dict=make_dict(lYear,lMonth)
+    dict['Calendar']=mark_safe(lCalendar)
+    dict['user_name']=user_name
+    return render_to_response('cal/month_user.html',dict)
